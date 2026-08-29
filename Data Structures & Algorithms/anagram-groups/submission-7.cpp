4f621f6a190d0806da92e73969e343c0991f9acd
Richard Nguyen <rich.nguyen20941@gmail.com>
2026-08-29 14:58:07 -0800

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagrams;
+        vector<vector<string>> res;
+
+        for (string s : strs){
+            vector<int> count(26);
+            for (char c : s){
+                count[c-'a']++;
+            }
+            string key = "";
+            for (int n : count){
+                key += to_string(n) + ',';
+            }
+            anagrams[key].push_back(s);
+        }
+
+        for (auto& pair : anagrams){
+            res.push_back(pair.second);
+        }
+        return res;
+    }
+};
