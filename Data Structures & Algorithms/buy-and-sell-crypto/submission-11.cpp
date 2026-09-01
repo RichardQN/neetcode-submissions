656e5f968aca946182ede809e217752f7e108d44
Richard Nguyen <rich.nguyen20941@gmail.com>
2026-09-01 10:33:55 -0800

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int l = 0, r = 0;
+        while (r < prices.size()){
+            if (prices[r] > prices[l]){
+                int day = prices[r] - prices[l];
+                profit = max(profit, day);
+            } else {
+                l = r;
+            }
+            r++;
+            
+        }
+        return profit;
+    }
+};
