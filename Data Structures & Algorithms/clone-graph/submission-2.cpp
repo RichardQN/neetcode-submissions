6a27c1809a7ba14354926e4812a16b177e987239
Richard Nguyen <rich.nguyen20941@gmail.com>
2026-09-15 22:42:16 -0800

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.cpp
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*, Node*> clone;
+        return dfs(node, clone);
+        
+    }
+
+    Node* dfs(Node* node, unordered_map<Node*, Node*>& clone){
+        if (!node){
+            return nullptr;
+        }
+
+        if (clone.contains(node)){
+            return clone[node];
+        }
+
+        Node* c = new Node(node->val);
+        clone[node] = c;
+        
+        for (Node* neighbor : node->neighbors){
+            c->neighbors.push_back(dfs(neighbor, clone));
+        }
+
+        return c;
+    }
+};
