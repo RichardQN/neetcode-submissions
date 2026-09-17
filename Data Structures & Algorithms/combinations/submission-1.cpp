19b140739a12faca69a9cc456a5e61eaa0a4fc33
Richard Nguyen <rich.nguyen20941@gmail.com>
2026-09-16 23:28:35 -0800

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    vector<vector<int>> res;
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> curr;
+        dfs(1, curr, n, k);
+        return res;
+    }
+
+    void dfs(int len, vector<int>& curr, int n, int k){
+        if (curr.size() == k){
+                res.push_back(curr);
+                return;
+            }
+        if (len > n){ 
+            return;
+        }
+        
+
+        curr.push_back(len);
+        dfs(len+1, curr, n, k);
+        curr.pop_back();
+        dfs(len + 1, curr, n, k);
+
+
+    }
+};
